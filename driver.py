a28a3504ee7166404794d33fcb4bf0f9d3e871a2
Andrew Mou <andrew.mou99@gmail.com>
2024-06-27 23:54:53 +0800

    added plot utilities

--- driver.py
+++ driver.py
@@ -9,7 +9,7 @@ game = MMGame(player_mms, coin_weights)
 player_win_probabilities, tie_probability, turn_counts = game.run_trials(num_trials)
 print(f'Win probabilities: {player_win_probabilities}')
 print(f'Tie probability: {tie_probability}')
-print(f'E[turns]: {sum(turn_counts) / num_trials}')\
+print(f'E[turns]: {sum(turn_counts) / num_trials}')
 
 plot_win_probabilities(len(player_mms), player_win_probabilities)
 plot_turn_count_distribution(num_trials, turn_counts)
